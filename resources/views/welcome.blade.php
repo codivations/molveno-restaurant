078e7c5b0a1b77dac0a88872f30f0426cf5d43ed
e10ne <etiennevanhalteren@hotmail.com>
2024-02-02 09:44:41 +0100

    Run fix-prettier

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -726,10 +726,10 @@
                 <div class="z-10 p-6 text-right sm:fixed sm:right-0 sm:top-0">
                     @auth
                         <a
-                            href="{{ url("/home") }}"
+                            href="{{ url("/dashboard") }}"
                             class="font-semibold text-gray-600 hover:text-gray-900 focus:rounded-sm focus:outline focus:outline-2 focus:outline-red-500 dark:text-gray-400 dark:hover:text-white"
                         >
-                            Home
+                            Dashboard
                         </a>
                     @else
                         <a
