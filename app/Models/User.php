a1c9ed3dca78dbd50b843a6052c275d9dc8ed5c4
e10ne <etiennevanhalteren@hotmail.com>
2024-02-23 10:27:21 +0100

    Replace email with login_name

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = ["name", "email", "password"];
+    protected $fillable = ["name", "login_name", "password"];
 
     /**
      * The attributes that should be hidden for serialization.
@@ -34,7 +34,6 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
-        "email_verified_at" => "datetime",
         "password" => "hashed",
     ];
 }
