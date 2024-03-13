a74601ef1d42e579e96197281e6b35bd86e8cf3b
e10ne <etiennevanhalteren@hotmail.com>
2024-03-13 15:18:02 +0100

    Add background color to menu

--- resources/views/orders/orderMenu.blade.php
+++ resources/views/orders/orderMenu.blade.php
@@ -1,7 +1,7 @@
 <nav class="flex w-full">
     @foreach ($menuNames as $menu)
         <a
-            class="{{ $currentService === $menu->service ? "border-b-4 border-b-blue-500" : "" }} mb-4 border p-4"
+            class="{{ $currentService === $menu->service ? "border-b-4 border-b-blue-500" : "" }} mb-4 border bg-white p-4"
             href="/order/{{ $tableNumber }}/{{ $menu->service }}"
         >
             {{ $menu->service }}
