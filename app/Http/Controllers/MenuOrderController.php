9c13a32df12daa19218b9f55b8b21b44a20ac085
e10ne <etiennevanhalteren@hotmail.com>
2024-03-13 13:15:12 +0100

    Add showService function and update index function

--- app/Http/Controllers/MenuOrderController.php
+++ app/Http/Controllers/MenuOrderController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Menu;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
@@ -9,6 +10,23 @@ class MenuOrderController extends Controller
 {
     public function index(): View
     {
-        return view("orders.index");
+        $menu = Menu::with("items")->get();
+
+        return view("orders.index", compact("menu"));
+    }
+
+    public function showService(
+        string $tableNumber,
+        string $currentService
+    ): View {
+        $menuNames = Menu::all("service");
+        $menu = Menu::where("service", $currentService)
+            ->with("items")
+            ->first();
+
+        return view(
+            "orders.service",
+            compact("menu", "menuNames", "tableNumber", "currentService")
+        );
     }
 }
