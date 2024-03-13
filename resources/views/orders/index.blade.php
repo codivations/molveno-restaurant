088409be6c86b2fd05468aea35af4ab227ccdf5a
e10ne <etiennevanhalteren@hotmail.com>
2024-03-13 13:16:00 +0100

    Update order index blade to show all menu items

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -3,7 +3,28 @@
 @section("title", "view menu")
 
 @section("content")
-    <div>
-        <h1 class="text-4xl">Hello Orders</h1>
-    </div>
+    <main class="min-h-screen bg-gray-300 p-4 text-black">
+        <div class="flex flex-row justify-between gap-4">
+            @foreach ($menu as $section)
+                <section class="flex-1">
+                    <button
+                        class="m-2 rounded-md border border-black bg-white px-4 py-3"
+                    >
+                        {{ $section->service }}
+                    </button>
+                    <div class="flex flex-col gap-2">
+                        @foreach ($section->items as $item)
+                            <article
+                                class="flex-1 rounded-md border border-black bg-white p-4"
+                            >
+                                <h3 class="text-2xl">{{ $item->name }}</h3>
+                                <p>{{ $item->getPrice() }}</p>
+                                <p>{{ $item->description }}</p>
+                            </article>
+                        @endforeach
+                    </div>
+                </section>
+            @endforeach
+        </div>
+    </main>
 @endsection
