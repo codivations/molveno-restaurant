f6f4bbfd8c506bb97ee4555597fc0e4d73a9c303
e10ne <etiennevanhalteren@hotmail.com>
2024-03-13 15:19:21 +0100

    Add styling to menu items

--- resources/views/orders/service.blade.php
+++ resources/views/orders/service.blade.php
@@ -3,12 +3,23 @@
 @section("title", "orders: $menu->service")
 
 @section("content")
-    @include("orders.orderMenu")
-    <section class="flex flex-col gap-2">
-        @foreach ($menu->items as $item)
-            <article class="flex-1 rounded-md border border-black bg-white p-4">
-                <h3 class="text-2xl">{{ $item->name }}</h3>
-            </article>
-        @endforeach
-    </section>
+    <div class="bg-gray-200">
+        @include("orders.orderMenu")
+        <section class="mx-2 my-2 flex flex-col gap-2">
+            @foreach ($menu->items as $item)
+                <article
+                    class="flex flex-1 justify-between rounded-md border border-black bg-white p-4"
+                >
+                    <div class="flex w-5/6 flex-wrap justify-between">
+                        <h3 class="w-1/2 text-2xl">{{ $item->name }}</h3>
+                        <p class="w-1/2">{{ $item->getPrice() }}</p>
+                        <p class="w-full">{{ $item->description }}</p>
+                    </div>
+                    <button type="button" class="">
+                        <x-add-circle-icon />
+                    </button>
+                </article>
+            @endforeach
+        </section>
+    </div>
 @endsection
