1972570a29468776ad38f8b7863bf33356d8f39a
e10ne <etiennevanhalteren@hotmail.com>
2024-03-27 15:43:35 +0100

    Add icon to project for menu

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -2,4 +2,7 @@
     <x-nav-button :href="__('/order')">
         <x-home-icon />
     </x-nav-button>
+    <x-nav-button :href="__('/order/' . $tableNumber . '/drinks')">
+        <x-menu-icon />
+    </x-nav-button>
 </nav>
