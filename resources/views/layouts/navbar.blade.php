f6b8b7b7f2c597cccc52aa9b163c8f093d65aeeb
e10ne <etiennevanhalteren@hotmail.com>
2024-03-29 12:19:13 +0100

    Add active prop for most buttons

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,11 +1,17 @@
-<nav class="flex pb-6">
-    <x-nav-button :href="__('/order')">
+<nav class="flex gap-2 bg-black p-2">
+    <x-nav-button :href="__('/order')" :active="__(Route::is('order.index'))">
         <x-home-icon />
     </x-nav-button>
-    <x-nav-button :href="__('/order/' . $tableNumber . '/drinks')">
+    <x-nav-button
+        :href="__('/order/' . $tableNumber . '/drinks')"
+        :active="__(Route::is('order.showService'))"
+    >
         <x-menu-icon />
     </x-nav-button>
-    <x-nav-button :href="__('/order/' . $tableNumber . '/showOrder')">
+    <x-nav-button
+        :href="__('/order/' . $tableNumber . '/showOrder')"
+        :active="__(Route::is('order.showOrder'))"
+    >
         <x-order-icon />
     </x-nav-button>
     <x-nav-button :href="__('/reservations')">
