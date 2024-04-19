cc4152b646066e45533e20b7210d669b5aa5248e
t-molder <67382928t-molder@users.noreply.github.com>
2024-04-19 12:00:55 +0200

    add kitchen status functions, WIP

--- app/Http/Controllers/KitchenController.php
+++ app/Http/Controllers/KitchenController.php
@@ -5,14 +5,84 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Order;
 use App\Models\OrderedItem;
+use App\Enums\ItemStatus;
+use Hamcrest\Text\StringContains;
 
 class KitchenController extends Controller
 {
     public function show()
     {
         $orders = Order::getAllOrders();
-
-        // $ordered_items = OrderedItem::getAllOrderedItems();
         return view("kitchen", compact(["orders"]));
+    }
+
+    public function progressItemStatus(string $itemId)
+    {
+        $orderedItem = OrderedItem::find($itemId);
+
+        switch ($orderedItem->status) {
+            case "to do":
+                $orderedItem->status = "in progress";
+                $orderedItem->save();
+
+                $order = $orderedItem->order;
+                if ($order->status == "to do") {
+                    $this->setOrderStatus($order, "in progress");
+                }
+
+                break;
+
+            case "in progress":
+                $orderedItem->status = "done";
+                $orderedItem->save();
+
+                $order = $orderedItem->order;
+                if ($this->isOrderDone($order)) {
+                    $this->setOrderStatus($order, "ready");
+                }
+                break;
+
+            case "done":
+                $orderedItem->status = "to do";
+                $orderedItem->save();
+                $order = $orderedItem->order;
+                if ($order->status == "ready") {
+                    $this->setOrderStatus($order, "in progress");
+                }
+                break;
+
+            default:
+                $msg = "I Am Error";
+                break;
+        }
+        return back();
+    }
+
+    public function setOrderStatus(Order $order, string $status)
+    {
+        $order->status = $status;
+        $order->save();
+    }
+
+    public function isOrderDone(Order $order)
+    {
+        $orderedItems = $order->orderedItems;
+
+        $orderID = $order->id;
+        foreach ($orderedItems as $item) {
+            if ($item->status != ItemStatus::DONE->value) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public function closeOrder(string $id)
+    {
+        $order = Order::find($id);
+        $order->status = "done";
+        $order->save();
+
+        return back();
     }
 }
--- resources/views/kitchen.blade.php
+++ resources/views/kitchen.blade.php
@@ -1,50 +1,81 @@
 @extends("layouts.main")
+{{-- @include("sections.kitchen.statusButton") --}}
 @section("title", "kitchen display")
 @section("content")
     {{--
         TODO
         Todo/inprogress/done checks
-        Have order top level sticky as well
         Make sum total overview
         Figure out how done orders get gone
-        Give it table number instead of order ID
         Allow multiples of one item in an order
     --}}
-
-    <div class="h-full bg-slate-400">
+    <div class="flex max-h-screen min-h-screen flex-col">
         <div class="topbar"></div>
         <div
-            class="overview flex-flow-col inline-flex flex-shrink-0 overflow-auto"
+            class="max-w-100 flex-flow-col flex max-h-dvh flex-auto flex-shrink-0 overflow-auto bg-slate-400"
         >
             @foreach ($orders as $order)
                 <div
-                    class="m-1 h-min min-w-72 rounded-lg border border-solid border-black bg-gray-600"
+                    class="min-h-content m-1 h-fit max-h-[100vh] min-w-72 rounded-lg border border-solid border-black bg-gray-600"
                 >
                     <div
-                        class="grid grid-cols-2 grid-rows-2 justify-between gap-1 rounded-lg border border-solid border-black bg-teal-300 bg-opacity-40 p-2 font-bold uppercase text-white"
+                        class="grid grid-cols-2 grid-rows-2 justify-between gap-1 rounded-lg border-2 border-solid border-black bg-teal-300 bg-opacity-40 p-2 font-bold uppercase text-white"
                     >
                         <div>{{ $order->created_at->format("H:i") }}</div>
                         {{-- <div>{{ $order->staff_id }}</div> --}}
                         <div>{{ $order->user->name }}</div>
-                        <div>{{ $order->id }}</div>
-                        <div>{{ $order->status }}</div>
+                        {{-- <div>{{ $order->id }}</div> --}}
+                        <div>
+                            Table {{ $order->table()->table_number ?? "none" }}
+                        </div>
+                        <div class="flex gap-9">
+                            {{ $order->status }}
+                            @if ($order->status == "ready")
+                                <a
+                                    href="/kitchen/close/id/{{ $order->id }}"
+                                    class="button m-0 h-6 w-6 rounded-sm p-0"
+                                >
+                                    &#x2714;
+                                </a>
+                            @endif
+                        </div>
                     </div>
 
-                    @foreach ($order->orderedItems as $order_item)
-                        @php
-                            $food = $order_item->item
-                        @endphp
+                    <ul class="flex h-fit max-h-[85vh] flex-col overflow-auto">
+                        @foreach ($order->orderedItems as $order_item)
+                            @php
+                                $item = $order_item->item
+                            @endphp
 
-                        <div
-                            class="m-1 rounded-lg border border-solid bg-white p-1"
-                        >
-                            <div class="font-bold uppercase">
-                                {{ $food->name }}
-                            </div>
-                            <div>NOTES: {{ $order_item->notes }}</div>
-                            <div>{{ $order_item->status }}</div>
-                        </div>
-                    @endforeach
+                            <li>
+                                <div
+                                    class="m-1 flex flex-col rounded-lg border border-solid bg-white p-1"
+                                >
+                                    <div class="font-bold uppercase">
+                                        {{ $item->name }}
+                                    </div>
+                                    <div class="">
+                                        {{-- This is nasty, needs work --}}
+                                        @if ($order_item->dietary_restrictions == "1")
+                                            <p>Allergy warning</p>
+                                        @endif
+
+                                        NOTES: {{ $order_item->notes }}
+                                    </div>
+
+                                    <div class="m-2 flex self-end p-2">
+                                        <a
+                                            href="/kitchen/progress/id/{{ $order_item->id }}"
+                                            class="button m-0 h-6 w-6 rounded-sm p-0"
+                                        >
+                                            &#x2714;
+                                        </a>
+                                    </div>
+                                    <div>{{ $order_item->status }}</div>
+                                </div>
+                            </li>
+                        @endforeach
+                    </ul>
                 </div>
             @endforeach
         </div>
