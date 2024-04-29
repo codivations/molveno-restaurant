048c88fda1ee9bf51032e98facc65f27061458f2
t-molder <67382928t-molder@users.noreply.github.com>
2024-04-29 15:24:55 +0200

    add table overview aesthetics

--- resources/views/tables/sections/seatedFilter.blade.php
+++ resources/views/tables/sections/seatedFilter.blade.php
@@ -1,7 +1,7 @@
 <div class="grid w-full grid-flow-col justify-stretch">
     @foreach ($items as $item)
         <a
-            class="{{ $currentSelection === $item ? "border-b-4 border-b-blue-500" : "" }} shrink-0 rounded-b-lg border border-black bg-white px-2 pb-4 pt-5 text-center font-semibold first-letter:uppercase"
+            class="{{ $currentSelection === $item ? "border-b-4 border-b-blue-500" : "" }} shrink-0 rounded-b-lg border border-black bg-white px-2 pb-4 pt-4 text-center font-semibold first-letter:uppercase"
             style="margin-top: -8px"
             href="{{ "/tables/" . $route . "/" . ($currentSelection === $item ? "all" : $item) }}"
         >
