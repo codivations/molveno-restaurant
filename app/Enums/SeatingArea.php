1b4501dc559506f17e29d648213fc0c02dad0e33
e10ne <etiennevanhalteren@hotmail.com>
2024-05-03 09:41:12 +0200

    Remove commented out line

--- app/Enums/SeatingArea.php
+++ app/Enums/SeatingArea.php
@@ -6,7 +6,6 @@ enum SeatingArea: string
 {
     case ALL = "all";
     case TERRACE = "terrace";
-    //case RESTAURANT = "restaurant";
     case GROUNDFLOOR = "ground floor";
     case FIRSTFLOOR = "first floor";
 }
